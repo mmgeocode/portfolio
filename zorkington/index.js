2ff97f405e2b58e91def74d8d387b4abdd4c1313
mmgeocode <mike.muncy85@gmail.com>
2024-09-10 10:55:53 -0600

    Adds storyboard but multiple bugs identified

--- zorkington/index.js
+++ zorkington/index.js
@@ -26,28 +26,42 @@ class Room {
     }
 }
 
-const start = new Room("start", "This is the starting room", ['bolt'], false, null, 'note', '1234', false, null, null)
-const middle = new Room("middle", "This is the middle room", [], true, "1234", null, null, false, null, null)
-const exit = new Room("exit", "This is the exit room", [], false, null, null, null, true, 'bolt', 'Place for a bolt')
+// Rooms
+const bedroom = new Room("bedroom", 'The walls and floor appear to be made of wet paint in a variety of bright colors. However, the paint does not smear when pressing against the surface. Located on a wall is a framed PHOTOGRAPH. There is a door to the HALLWAY with a small speaker near the handle... "who puts a speaker on a door?"', [], false, null, 'photograph', 'A black and white photograph but the colors are opposite. The title on a small plaque is, "Speak Opposite to Open".', false, null, null)
+
+const hallway = new Room("hallway", "The floor, walls, and ceiling of this hallway is covered in orange shag carpet. You see a LAMP resting on a small table in the middle of the hallway. There are entrances to a GUEST bedroom, a BATHROOM, an OFFICE, and a pathway that leads to the KITCHEN and LIVING room.", ['key'], true, 'close', 'lamp', 'You examine the lamp and find nothing out of place. After turning the lamp on, the colors change in progression of a rainbow. Red, Orange, Yellow, Blue, Green, Indigo, and then Violet', false, null, null)
+
+const kitchen = new Room('kitchen', 'This kitchen is made entirely out of cardboard... nothing actually works. Everything is essentially a prop with no functional use. You see a door leading to the GARAGE and pathways to the LIVING room and HALLWAY. Some of the food around the kitchen may actually be real', ['carrot'], false, null, null, null, false, null, null)
+
+const living = new Room('living', "You have never seen so many types of wood paneling covering everything before. Even the furniture is completely made out of wood and looks very uncomfortable. In the center of the room, there is a massive metal DOOR with what appears to be blue eyeballs.", [], false, null, 'door', 'As you approach the scary door it begins to speak. "BE GONE FROM MINE HOME THY VILE CREATURE! GO LOOK AT THINE FOULNESS IN A MIRROR!"', false, null, null)
+
+const guest = new Room('guest', "If any guests have ever stayed here before, the most likely would have been disturbed by the gothic decor. Black stone covered the floors, walls, and ceiling complete with black metal spikes. You dare not attempt to lie down on the charcoal colored bed. There may be something in the nightstands.", ['keycard'], false, null, null, null, true, 'carrot', 'There is a sculpture of a rabbit with an open mouth where the doorhandle should be. Place something in the mouth?: ')
+
+const bathroom = new Room("bathroom", "This is the exit room", [], false, null, null, null, true, 'key', 'A keyhole')
 
 // State Machines
-let locationCurrent = "start"
+let locationCurrent = "bedroom"
 
 const player = {
     name: '',
-    inventory: ['dice', 'tea']
+    inventory: []
 }
 
 const locationLookup = {
-    start,
-    middle,
-    exit
+    bedroom,
+    hallway,
+    kitchen,
+    living,
+    bathroom
 }
 
 const locationStates = {
-    start: ["middle"],
-    middle: ["start", "exit"],
-    exit: ["middle"]
+    bedroom: ["hallway"],
+    hallway: ["bedroom", "bathroom", "kitchen", "living", "guest"],
+    guest: ["hallway"],
+    kitchen: ["hallway", "living", "garage"],
+    living: ["hallway", "kitchen"],
+    bathroom: ["hallway"]
 }
 
 // Move Location Function
@@ -108,10 +122,10 @@ function dropItem(item) {
 
 // Examine Immutable Object Function
 function examineObject(object) {
-    if (locationLookup[locationCurrent].imobject.includes(object)) {
-        console.log(`${locationLookup[locationCurrent].imobjdesc}`)
+    if (locationLookup[locationCurrent].roomObject.includes(object)) {
+        console.log(`${locationLookup[locationCurrent].roomObjectDescription}`)
     } else {
-        console.log(`Nothing to examine`)
+        console.log(`You find nothing to examine`)
     }
     askInput()
 }
@@ -119,7 +133,7 @@ function examineObject(object) {
 // Unlock Door
 async function unlockDoor(room) {
     if (locationStates[locationCurrent].includes(room) && locationLookup[room].lock == true) {
-        lockResponse = await ask(`What is the correct code: `)
+        lockResponse = await ask(`What is the password: `)
         if (lockResponse == locationLookup[room].lockCode) {
             locationLookup[room].lock = false
             console.log(`The door is now unlocked`)
@@ -146,6 +160,7 @@ async function unlockDoor(room) {
 function helpPlayer() {
     console.log('type MOVE + ROOM NAME to move between rooms')
     console.log('type SEARCH to search the current room')
+    console.log('type EXAMINE + ROOM OBJECT to investigate an object within the room')
     console.log('type INVENTORY to check your inventory')
     console.log('type TAKE + ITEM NAME to pick up an item from the room')
     console.log('type DROP + ITEM NAME to drop an item into the current room')
@@ -160,9 +175,10 @@ async function askInput() {
         console.log(`Congratulations ${player.name}! You found the exit`)
         process.exit()
     } else {
-        console.log(`You're at the ${locationLookup[locationCurrent].name} and it's ${locationLookup[locationCurrent].description}`)
+        console.log(`You're in the ${locationLookup[locationCurrent].name}. ${locationLookup[locationCurrent].description}`)
         response = await ask(`Input your action. Type 'help' for a list of actions.\n>_`)
         answer = response.toLowerCase().split(' ')
+        console.clear()
         if (answer[0] === 'move') {
             moveLocation(answer[1])
         } else if (answer[0] === 'search') {
@@ -188,8 +204,9 @@ async function askInput() {
 
 // Game Start Function
 async function gameStart() {
-    console.log(`Game Start: Version 0.1`)
-    const nameQuestion = await ask('What is your name?: ')
+    console.log(`The Scary Door: Version 0.2`)
+    console.log(`You wake up in a strange bedroom you have never seen before. The last thing you remember is eating the "Froot" salad from Fishy Joe's. A strange uneasyness feeling sinks in your stomach... "I've got to get out of here," you think to yourself.`)
+    const nameQuestion = await ask('But first... what is my name?: ')
     player.name = nameQuestion
     console.log(`Good luck ${player.name}!`)
     askInput()
